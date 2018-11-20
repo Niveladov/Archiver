c5bc1a0732e6e72229bb2f781ffc67636ce36d88
E.Fedoruk <E.Fedoruk@BS7714>
2018-11-20 13:44:04 +0300

    Add project files.

---
+++ archiver/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace archiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourceFile = @"D:/Niveladov/models.txt"; // исходный файл
+            string compressedFile = @"D:/Niveladov/models.gz"; // сжатый файл
+            string targetFile = @"D:/Niveladov/models_new.txt"; // восстановленный файл
+
+            // создание сжатого файла
+            Compress(sourceFile, compressedFile);
+            // чтение из сжатого файла
+            Decompress(compressedFile, targetFile);
+
+            Console.ReadLine();
+        }
+        
+        public static void Compress(string sourceFile, string compressedFile)
+        {
+            // поток для чтения исходного файла
+            using (FileStream sourceStream = new FileStream(sourceFile, FileMode.OpenOrCreate))
+            {
+                // поток для записи сжатого файла
+                using (FileStream targetStream = File.Create(compressedFile))
+                {
+                    // поток архивации
+                    using (GZipStream compressionStream = new GZipStream(targetStream, CompressionMode.Compress))
+                    {
+                        sourceStream.CopyTo(compressionStream); // копируем байты из одного потока в другой
+                        Console.WriteLine("Сжатие файла {0} завершено. Исходный размер: {1}  сжатый размер: {2}.",
+                            sourceFile, sourceStream.Length.ToString(), targetStream.Length.ToString());
+                    }
+                }
+            }
+        }
+
+        public static void Decompress(string compressedFile, string targetFile)
+        {
+            // поток для чтения из сжатого файла
+            using (FileStream sourceStream = new FileStream(compressedFile, FileMode.OpenOrCreate))
+            {
+                // поток для записи восстановленного файла
+                using (FileStream targetStream = File.Create(targetFile))
+                {
+                    // поток разархивации
+                    using (GZipStream decompressionStream = new GZipStream(sourceStream, CompressionMode.Decompress))
+                    {
+                        decompressionStream.CopyTo(targetStream);
+                        Console.WriteLine("Восстановлен файл: {0}", targetFile);
+                    }
+                }
+            }
+        }
+
+
+    }
+}
+
